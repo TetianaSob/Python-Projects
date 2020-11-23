1fce02f29014ac98032873eb06616be4d1d6ac15
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2020-11-22 19:45:47 -0500

    Regex Names
    Regex Names

---
+++ Regex_Names.py
@@ -0,0 +1,14 @@
+# Regex_Names.py
+import re
+
+def parse_name(input):
+    name_regex = re.compile(r'^(Mr\.|Mrs\.|Mdme\.) (?P<first>[A-Za-z]+) (?P<last>[A-Za-z]+)$')
+    matches = name_regex.search(input)
+    print(matches.group())
+    print(matches.group('first'))
+    print(matches.group('last'))
+
+parse_name("Mrs. Tilda Swinton") 
+# Mrs. Tilda Swinton
+# Tilda
+# Swinton

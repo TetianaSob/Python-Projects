a54d80b0da477d1b065066229831b33f54eaf78d
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2020-11-29 16:37:43 -0500

    Regex_Names
    Regex_Names

--- Regex_Names.py
+++ Regex_Names.py
@@ -1,14 +1,28 @@
-# Regex_Names.py
+# Regex_names.py
+
 import re
 
-def parse_name(input):
-    name_regex = re.compile(r'^(Mr\.|Mrs\.|Mdme\.) (?P<first>[A-Za-z]+) (?P<last>[A-Za-z]+)$')
-    matches = name_regex.search(input)
-    print(matches.group())
-    print(matches.group('first'))
-    print(matches.group('last'))
+text = "Last night Mrs. Daisy and Mr. White murdered Ms. Chow"
 
-parse_name("Mrs. Tilda Swinton") 
-# Mrs. Tilda Swinton
-# Tilda
-# Swinton
+pattern = re.compile(r'(Mr.|Mrs.|Ms.) [a-z]+', re.I)
+pattern.sub("REDACTED", text)
+
+print(pattern.search(text).group()) # Mrs. Daisy
+
+print("\n")
+
+text2 = "Last night Mrs. Daisy and Mr. White murdered Ms. Chow"
+
+pattern = re.compile(r'(Mr.|Mrs.|Ms.) [a-z]+', re.I)
+result2 = pattern.sub("REDACTED", text2)
+
+print(result2) # Last night REDACTED and REDACTED murdered REDACTED
+
+print("\n")
+
+text3 = "Last night Mrs. Daisy and Mr. White murdered Ms. Chow"
+
+pattern = re.compile(r'(Mr.|Mrs.|Ms.) [a-z]+', re.I)
+result3 = pattern.sub("\g<1>REDACTED", text3)
+
+print(result3) # Last night Mrs.REDACTED and Mr.REDACTED murdered Ms.REDACTED

9007ac158598be0639bd163fc9a43e58d1c6e019
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2021-02-10 16:18:26 -0500

    Add files via upload

---
+++ 1_data_types.py
@@ -0,0 +1,13 @@
+# 1_data_types.py
+
+primeNumbers = [3, 7 , 61, 29, 199]
+print(type(primeNumbers)) # <class 'list'>
+
+coolPeople = ['Jay z', 'Ghandi', 'me']
+primeAndPeople = ['King Arthur', 17, 11, 'Jennifer Lopez']
+print(type(primeAndPeople)) # <class 'list'>
+
+print(primeNumbers[2]) # 61
+
+bestPrimeEver = primeNumbers[4]
+print(bestPrimeEver) # 199
---
+++ 2_csv_file.py
@@ -0,0 +1,6 @@
+# 2_csv_file.py
+
+import pandas as pd
+data = pd.read_csv('lsd_math_score_data.csv')
+
+print(data)
